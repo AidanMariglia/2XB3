9f52672de22a7d1f1d4b99e8b1862124762affea
Aidan Mariglia <aidan_m@outlook.com>
2021-01-18 14:27:52 -0500

    implemented code.py

---
+++ Lab_01/code.py
@@ -0,0 +1,15 @@
+
+def are_valid_groups(studentNum, groups):
+  flag = True
+
+  for num in studentNum:
+    flag = False
+    
+    for group in groups:
+      if num in group:
+        flag = True
+      
+    if not flag:
+      return False
+
+  return True
