96f57fb795671d55cc404c2b19104fa396183f8e
BenDubi <duboisb@mcmaster.ca>
2021-01-28 17:15:09 -0500

    Copy test

--- Lab_02/lab_2.py
+++ Lab_02/lab_2.py
@@ -1,9 +1,11 @@
 import timeit
 import random
 
-def copy():
-    list1 = [1, 2, 3, 4, 5]
-    list2 = list1.copy()
+def copy(Length):
+    list1 = []
+    for i in range(Length):
+        list1.append(i)
+    
 
 def lookups():
     
@@ -15,7 +17,15 @@ def lookups():
     for i in range(1000000):
         x = L[i]
 
-    
+def timetest(runs, Length):
+    total = 0
+    for _ in range(runs):
+        start = timeit.default_timer()
+        copy(Length)
+        end = timeit.default_timer()
+        total += end - start
+    return total/runs
 
+for i in range(100, 10000, 100):
+    print(i, timetest(10, i))
 
-   
