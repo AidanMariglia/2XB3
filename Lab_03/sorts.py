1aa3ebefd88ac2e77d0e90cae03613054bde4037
Aidan Mariglia <aidan_m@outlook.com>
2021-02-03 10:43:34 -0500

    )

--- Lab_03/sorts.py
+++ Lab_03/sorts.py
@@ -9,8 +9,20 @@ def sort_rec(a, lo, hi):
     pass
 
 def partition(a, lo, hi):
-    pass
+    i = lo
+    j = hi + 1
+    pivot = a[lo]
 
+    while(1):
+        while (a[i + 1] < pivot):
+            i += 1
+            if (i == hi): break
+        
+        while (a[j - 1] > pivot):
+            j -= 1
+            if (j == lo): break
+        
+        if (i >= j): break
 def exchange(a, i, j):
     temp = a[i]
     a[i] = a[j]
