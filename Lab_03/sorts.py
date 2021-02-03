3f4e8c8b38de5ed67bf234d3259e81747b2c0084
Aidan Mariglia <aidan_m@outlook.com>
2021-02-03 10:29:42 -0500

    .

--- Lab_03/sorts.py
+++ Lab_03/sorts.py
@@ -8,4 +8,10 @@ def quicksort_inplace(a):
 def sort_rec(a, lo, hi):
     pass
 
-def partition()
+def partition(a, lo, hi):
+    pass
+
+def exchange(a, i, j):
+    temp = a[i]
+    a[i] = a[j]
+    a[j] = temp
