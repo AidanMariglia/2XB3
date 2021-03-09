b919d1297b37fb44b847608aed2a25008d459138
BenDubi <duboisb@mcmaster.ca>
2021-03-08 20:25:19 -0500

    add

--- Lab_07/code.py
+++ Lab_07/code.py
@@ -35,3 +35,21 @@ def test_DFS2():
     print(DFS2(test, 4, 2))
 
 test_DFS2()
+
+def test_DFS3():
+    test = Graph(6)
+
+    for i in range(1,7):
+        test.add_node()
+
+    test.add_edge(6,4)
+    test.add_edge(4,3)
+    test.add_edge(4,5)
+    test.add_edge(5,2)
+    test.add_edge(3,2)
+    test.add_edge(5,1)
+    test.add_edge(2,1)
+
+    print(DFS3(test, 4))
+
+test_DFS3()
--- Lab_07/graphs.py
+++ Lab_07/graphs.py
@@ -40,3 +40,22 @@ def DFS2(G, node1, node2):
                     return path[current_node] + [node]
                 S.append(node)
     return []
+
+def DFS3(G, node1):
+    path = {}
+    S = [node1]
+    marked = {}
+    
+    for node in G.adj:
+        marked[node] = False
+    
+    while len(S) != 0:
+        current_node = S.pop()
+        if not marked[current_node]:
+            marked[current_node] = True
+            for node in G.adj[current_node]:
+                if marked[node] == False:
+                    print(current_node)
+                    path[node] = current_node
+                S.append(node)
+    return path
