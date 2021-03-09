31cf42129385edf9eddff6d88bfd48e16b6b07cb
Aidan Mariglia <aidan_m@outlook.com>
2021-03-08 20:57:46 -0500

    hello

--- Lab_07/code.py
+++ Lab_07/code.py
@@ -41,34 +41,16 @@ def test_DFS2():
 
     print(DFS2(test, 4, 2))
 
-<<<<<<< HEAD
-test_DFS2()
-
-def test_DFS3():
-    test = Graph(6)
-
-    for i in range(1,7):
-        test.add_node()
-
-=======
 def test_is_connected():
     test = Graph(7)
     
     test.add_edge(0,1)
->>>>>>> 2d316bb5c58cf2fc5e6dd1ef03faf2cb236e6c33
     test.add_edge(1,2)
     test.add_edge(1,3)
     test.add_edge(2,4)
     test.add_edge(3,5)
     test.add_edge(3,4)
     test.add_edge(5,4)
-<<<<<<< HEAD
-    test.add_edge(4,6)
-
-    print(DFS3(test, 1))
-
-test_DFS3()
-=======
     test.add_edge(6,4)
     
     
@@ -76,4 +58,3 @@ test_DFS3()
 
 test_is_connected()
 
->>>>>>> 2d316bb5c58cf2fc5e6dd1ef03faf2cb236e6c33
