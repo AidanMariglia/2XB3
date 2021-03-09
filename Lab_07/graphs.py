c00abbd2d0494bd652c59959c7e5ce9d1c2c5013
BenDubi <duboisb@mcmaster.ca>
2021-03-08 19:56:27 -0500

    yeah

--- Lab_07/graphs.py
+++ Lab_07/graphs.py
@@ -21,5 +21,37 @@ def BFS2(G, node1, node2):
 
     return []
 
-def DFS2():
-    pass
+def DFS2(G, node1, node2):
+    path = []
+    S = [node1]
+    marked = {}
+    
+    for node in G.adj:
+        marked[node] = False
+    
+    while len(S) != 0:
+        current_node = S.pop()
+        if not marked[current_node]:
+            marked[current_node] = True
+            for node in G.adj[current_node]:
+                if marked[node] == False:
+                    path.append(current_node)
+                if node == node2:
+                    path.append(node2)
+                    return path
+                S.append(node)
+    return path
+
+
+
+new = Graph(10)
+
+new.add_edge(1, 2)
+
+new.add_edge(2, 5)
+
+new.add_edge(5, 8)
+
+new.add_edge(8, 3)
+
+print(DFS2(new, 1, 3))
