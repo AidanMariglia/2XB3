cd9e621a5116a92a9b62a62d74a7f20605b79ec3
Aidan Mariglia <aidan_m@outlook.com>
2021-03-17 19:45:06 -0400

    cleanup

--- Lab_08/code.py
+++ Lab_08/code.py
@@ -1,5 +1,31 @@
 from mst import *
 
+def prim1Test():
+    test = WeightedGraph(7)
+    test.add_edge(0, 1, 1)
+    test.add_edge(0, 2, 10)
+    test.add_edge(0, 3, 2)
+    test.add_edge(0, 5, 12)
+    test.add_edge(1, 2, 3)
+    test.add_edge(2, 3, 15)
+    test.add_edge(2, 4, 18)
+    test.add_edge(3, 4, 4)
+    test.add_edge(3, 5, 5)
+    test.add_edge(3, 6, 21)
+    test.add_edge(4, 6, 6)
+    test.add_edge(5, 6, 24)
+
+    test2 = prim1(test)
+
+    print(test2.number_of_nodes())
+    print(test2.adjacent_nodes(0))
+    print(test2.adjacent_nodes(1))
+    print(test2.adjacent_nodes(2))
+    print(test2.adjacent_nodes(3))
+    print(test2.adjacent_nodes(4))
+    print(test2.adjacent_nodes(5))
+    print(test2.adjacent_nodes(6))
+    
 def prim2Test():
     graph = WeightedGraph(4)
     graph.add_edge(0,1,1)
@@ -14,4 +40,8 @@ def prim2Test():
         print(mst.adjacent_nodes(i))
 
 
-prim2Test()
+def compare_test():
+    pass
+
+def generate_random_graph():
+    pass
--- Lab_08/mst.py
+++ Lab_08/mst.py
@@ -44,28 +44,3 @@ def prim2(G):
                 edges.decrease_key(node[0], node[1])
 
     return mst
-
-test = WeightedGraph(7)
-test.add_edge(0, 1, 1)
-test.add_edge(0, 2, 10)
-test.add_edge(0, 3, 2)
-test.add_edge(0, 5, 12)
-test.add_edge(1, 2, 3)
-test.add_edge(2, 3, 15)
-test.add_edge(2, 4, 18)
-test.add_edge(3, 4, 4)
-test.add_edge(3, 5, 5)
-test.add_edge(3, 6, 21)
-test.add_edge(4, 6, 6)
-test.add_edge(5, 6, 24)
-
-test2 = prim1(test)
-
-print(test2.number_of_nodes())
-print(test2.adjacent_nodes(0))
-print(test2.adjacent_nodes(1))
-print(test2.adjacent_nodes(2))
-print(test2.adjacent_nodes(3))
-print(test2.adjacent_nodes(4))
-print(test2.adjacent_nodes(5))
-print(test2.adjacent_nodes(6))
