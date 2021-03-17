5389e74f851526e826ddb8d3b0d008de14dafbce
Aidan Mariglia <aidan_m@outlook.com>
2021-03-17 13:50:04 -0400

    fuck yeah

--- Lab_08/mst.py
+++ Lab_08/mst.py
@@ -2,33 +2,29 @@ from lab8 import *
 from min_heap import *
 
 def prim2(G):
-
-    if G.number_of_nodes() == 0:
-        return G
-
-    #as the greatest possible edge weight is 1000,
-    #1001 will be used to represent infinity
     inf = 1001
-    A = {0}
+    edges = MinHeap([Element(node, inf) for node in range(1,G.number_of_nodes())])
     mst = WeightedGraph(G.number_of_nodes())
-    edges = MinHeap([Element(node, inf) for node in range(G.number_of_nodes())])
+    marked = [False for _ in range(G.number_of_nodes())]
 
+    for node in (G.adjacent_nodes(0)):
+        edges.decrease_key(node[0], node[1])
+    marked[0] = True
 
-    for node in G.adjacent_nodes(0):
-        edges.decrease_key(node[0], G.w(node[0], 0))
-    
-    prevMin = 0
     while not edges.is_empty():
         u = edges.extract_min()
-        A.add(u.value)
         for node in G.adjacent_nodes(u.value):
             if u.key == node[1]:
-                mst.add_edge(u.value, prevMin, G.w(u.value, prevMin))
-            if node[0] not in A and \
-                edges.get_element_from_value(node[0]).key > G.w(node[0], u.value):
-                    edges.decrease_key(node[0], G.w(node[0], u.value))
-        prevMin = u.value
+                mst.add_edge(node[0], u.value, u.key)
+                marked[u.value] = True
+                print("marked " + str(u.value))
+
+        for node in (G.adjacent_nodes(u.value)):
+            if not marked[node[0]]:
+                print("decreasing key " + str(node[0]))
+                edges.decrease_key(node[0], node[1])
 
     return mst
+
 
     
