70e9848484e8820e2425136c5d6bb5810ae444fd
BenDubi <duboisb@mcmaster.ca>
2021-03-17 21:33:55 -0400

    hmmm

--- Lab_08/mst.py
+++ Lab_08/mst.py
@@ -1,5 +1,6 @@
 from lab8 import *
 from min_heap import *
+from random import shuffle, randint
 
 def prim1(WGraph):
     if WGraph.number_of_nodes() == 0:
@@ -23,20 +24,42 @@ def prim2(G):
     edges = MinHeap([Element(node, inf) for node in range(1,G.number_of_nodes())])
     mst = WeightedGraph(G.number_of_nodes())
     marked = [False for _ in range(G.number_of_nodes())]
-
     for node in (G.adjacent_nodes(0)):
         edges.decrease_key(node[0], node[1])
     marked[0] = True
-
+    u = edges.extract_min()
     while not edges.is_empty():
-        u = edges.extract_min()
         for node in G.adjacent_nodes(u.value):
-            if u.key == node[1]:
-                mst.add_edge(node[0], u.value, u.key)
-                marked[u.value] = True
-
-        for node in (G.adjacent_nodes(u.value)):
-            if not marked[node[0]]:
-                edges.decrease_key(node[0], node[1])
-
+            edges.decrease_key(node[0], node[1])
+        u = edges.extract_min()
     return mst
+
+def generate_random_graph(nodes, edges):
+    edgeWeights = [i for i in range(1,1001)]
+    unVisitedNodes = [i for i in range(nodes)]
+    shuffle(edgeWeights)
+    shuffle(unVisitedNodes)
+    randomGraph = WeightedGraph(nodes)
+
+    currNode = unVisitedNodes.pop()
+    while len(unVisitedNodes) > 0:
+        nextNode = unVisitedNodes.pop()
+        randomGraph.add_edge(currNode, nextNode, edgeWeights.pop())
+        currNode = nextNode
+
+    for i in range(edges - (nodes + 1)):
+
+        node1 = randint(0, nodes - 1)
+        node2 = randint(0, nodes - 1)
+
+        while node1 == node2 or randomGraph.are_connected(node1, node2):
+            node1 = randint(0, nodes - 1)
+            node2 = randint(0, nodes - 1)
+
+        randomGraph.add_edge(node1, node2, edgeWeights.pop())
+
+    return randomGraph
+
+test = generate_random_graph(50, 50)
+
+prim2(test)
