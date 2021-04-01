153705164aa18f46dba22d6a608a0b6d7000cbab
Aidan Mariglia <aidan_m@outlook.com>
2021-03-31 20:24:19 -0400

    p

--- Lab_10/code.py
+++ Lab_10/code.py
@@ -1,1 +1,13 @@
 from vertex_cover import *
+
+
+def test_vc_approx_1(nodes, edges):
+    print("approx_length, real_length")
+
+    for i in range(10, edges, 10):
+        g = create_random_graph(nodes, i)
+        print(len(vc_approx1(g)), end=',')
+        print(len(vertex_cover(g)))
+
+
+test_vc_approx_1(20, 190)
